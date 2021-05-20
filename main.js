ee734817e76ed3aba4a97c7e707efcb8f8c1e8b4
Christine Antonio <christinejm.antonio@gmail.com>
2021-05-20 16:51:56 -0700

    4. seraching in a bst

--- main.js
+++ main.js
@@ -215,7 +215,20 @@ const main = () => {
     };
   
     console.log(bookFinder(LIBRARY, "000.12", "Computer Science"));
-  
+
+    // 4. Searching in a BST
+
+    /* 1) Given a  binary search tree whose in-order and pre-order traversals 
+    are respectively 14 15 19 25 27 35 79 89 90 91 and 35 25 15 14 19 27 89 79 91 90. 
+    What would be its postorder traversal? */
+
+    // 14 19 15 27 25 79 90 91 89 35
+
+    /*2) The post order traversal of a binary search tree is 5 7 6 9 11 10 8. 
+    What is its pre-order traversal?*/
+
+    // 8 6 5 7 9 10 11
+
 };
 
 module.exports = main;
